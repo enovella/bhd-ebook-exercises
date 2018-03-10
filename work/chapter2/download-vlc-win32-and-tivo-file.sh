0125cf28435e752c67e326eaeea8d1ac030825f6
Eduardo Novella <ednolo@inf.upv.es>
2018-03-10 21:22:12 +0100

    chapter2: back to the 90's: Exploiting VLC & TiVo video files. Stack-based buffer overflow

---
+++ work/chapter2/download-vlc-win32-and-tivo-file.sh
@@ -0,0 +1,3 @@
+wget https://get.videolan.org/vlc/0.9.4/vlc-0.9.4.tar.bz2
+wget http://samples.mplayerhq.hu/TiVo/test-dtivo-junkskip.ty%2b
+wget https://download.videolan.org/vlc/0.9.4/win32/vlc-0.9.4-win32.exe
